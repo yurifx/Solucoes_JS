12f6ca730335a759241472e598e1739150f82397
Yuri Vasconcelos <yurifx@hotmail.com>
2017-12-29 11:46:36 -0200

    Ajustes realizados

---
+++ variables.js
@@ -0,0 +1,68 @@
+console.log('Iniciando o projeto Variáveis');
+
+// o let tem escopo local, já o var tem escopo global
+
+// strings | texts
+var texto = "Variável texto";
+let texto2 = "Nova variavel";
+
+// numbers 
+var numeroInteiro = 10;
+var numeroDecimal = 10.2;
+
+//objects
+let obj = {};
+obj.nome = 'Yuri';
+obj.age = 20;
+
+let obj2 = {
+    nome:"John",
+    age: 20
+}
+
+//Arrays | Lists
+let list = [];
+lista = ["item1", "item2"];
+
+let list2 = ["oi", 2];
+
+//functions | types
+let func = function(name, age){
+    this.name = name;
+    this.age = age;
+    console.log('name is: ' + name + ' age is: ' + age);
+}
+
+// calling func variable that is a function
+func('calling func', 30);
+
+// array of objects of objectrs
+let complex = [
+    { model: {
+            name: "yuri",
+            age: 20
+        },
+      type: "complex"
+    }, 
+    {model:
+        {
+            name: "John",
+            age: 30
+        },
+        type: "complex"
+    }
+];
+
+//try
+
+
+
+
+
+
+
+
+
+
+
+console.log('debug finalized');
