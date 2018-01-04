53f7ef9d2849c17ba18373df6cf297accb773c2a
Yuri Vasconcelos <yurifx@hotmail.com>
2018-01-04 09:06:39 -0200

    let vs var in variables.js differences

--- variables.js
+++ variables.js
@@ -23,7 +23,7 @@ obj.nome = 'Yuri';
 obj.age = 20;
 
 let obj2 = {
-    nome:"John",
+    nome: "John",
     age: 20
 }
 
@@ -35,7 +35,7 @@ lista = ["item1", "item2"];
 let list2 = ["oi", 2];
 
 //functions | types
-let func = function(name, age){
+let func = function (name, age) {
     this.name = name;
     this.age = age;
     console.log('name is: ' + name + ' age is: ' + age);
@@ -45,15 +45,15 @@ let func = function(name, age){
 func('calling func', 30);
 
 // array of objects of objectrs
-let complex = [
-    { model: {
+let complex = [{
+        model: {
             name: "yuri",
             age: 20
         },
-      type: "complex"
-    }, 
-    {model:
-        {
+        type: "complex"
+    },
+    {
+        model: {
             name: "John",
             age: 30
         },
@@ -62,3 +62,27 @@ let complex = [
 ];
 
 console.log('debug finalized');
+
+
+//Diferenças entre var e let.
+
+// 1: Uma variável em escopo global declarada com let não é exportada como global, o var sim.
+// exemplo: https://jsfiddle.net/a74pz3v4/
+
+// 2: só pode ser usada na linha seguinte já o var pode ser precedido.
+
+// console.log(varlet); 
+// let varlet = "variavel let"; //dá erro
+
+console.log(varvar);
+var varvar = "variavel var"; //executa corretamente.
+
+// 3: o let é usado para laços de repetição, onde a variável é finalizada após seu uso.
+
+function TesteLista() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+
+    //console.log(i);  //o i já morreu (uncaught reference error)
+}
