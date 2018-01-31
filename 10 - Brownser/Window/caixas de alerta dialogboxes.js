07d101e62a19bb0df858894415900a7a7960b210
yvs <yurifx@hotmail.com>
2018-01-31 16:20:44 -0200

    Update caixas de alerta dialogboxes.js

--- 10 - Brownser/Window/caixas de alerta dialogboxes.js
+++ 10 - Brownser/Window/caixas de alerta dialogboxes.js
@@ -2,3 +2,9 @@ keys: dialog, caixas, box, alertas, confirm
 
 alert('oi');
 confirm('oi');
+
+var apagar = confirm('deseja realmente excluir este registro?');
+	if (apagar) {
+} else {
+	event.preventdefault();
+}
